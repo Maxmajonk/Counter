a64dc9fc1314c97d22a151f16919adb5d3174ac1
Maxmajonk <maxmajonk@gmail.com>
2023-03-05 02:47:15 +0100

    С помощью StoryBourd создал Label и Button. Кнопка "Нажми меня" увеличивает значения счётчика на 1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var znachOchka: UILabel!
+    var count = 0
+    @IBAction func Ochk(_ sender: Any) {
+        count += 1
+        znachOchka.text = "Значение счётчика: \(count)"
+    }
 }
 
